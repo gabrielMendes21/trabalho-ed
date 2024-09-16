2d5cdbb9f01df19a10448c20539ac89b5caff928
Gabriel Mendes <gabriel.26056@gmail.com>
2024-09-16 14:31:00 -0300

    finishing func

--- utils/cadastrar_alunos.h
+++ utils/cadastrar_alunos.h
@@ -1,11 +1,6 @@
 #ifndef CADASTRAR_ALUNOS_H
 #define CADASTRAR_ALUNOS_H
 
-bool cadastrar_alunos(Aluno alunos[], int *qtde_alunos)
-{
-    // Ler o arquivo `aluno.txt` e criar um dado do tipo `Aluno` para cada um
-    // Não se esqueca de atualizar a quantidade de alunos no vetor
-    return true;
-}
+bool cadastrar_alunos(Aluno alunos[], int *qtde_alunos);
 
 #endif
