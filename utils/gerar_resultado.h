0ac6256ef7dc068c5a4a1346fe5ea78e1d860ebb
Brigulinoo07 <paulo.silva1449@etec.sp.gov.br>
2024-09-22 23:29:45 -0300

    gerar resultado

--- utils/gerar_resultado.h
+++ utils/gerar_resultado.h
@@ -1,11 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
 
 #ifndef GERAR_RESULTADO_H
 #define GERAR_RESULTADO_H
 
-bool gerar_resultado(TG projetos[], Eleitor comissao[], Aluno formandos[])
+bool gerar_resultado(TG projetos[], Eleitor comissao[], Aluno formandos[], int tamanho_vet)
 {
-    // Gere o arquivo `resultado.txt` com o resultado baseado nos votos até o momento
+    //abre o arquivo
+    FILE *arquivo = fopen("resultado.txt", "w");
+    //verifica se o arquivo foi criado
+    if (arquivo == NULL){
+        printf("Erro ao criar o arquivo\n");
+        return false;
+    }
+    //escreve o número de votos em cada projeto
+    fprintf(arquivo, "Resultado da votação:\n");
+    for(int i = 0; i < tamanho_vet; i++){
+        fprintf(arquivo, "Projeto: %s, votos: %d\n", projetos[i]. titulo, projetos[i].qtde_votos);
+    }
+
+    fclose(arquivo);
+
     return true;
+
 }
 
 #endif
