c0c7a598f38d7a2a03aee95d98e2ae718ad36965
Gabriel Mendes <gabriel.26056@gmail.com>
2024-09-23 16:38:35 -0300

    gabriel: add suspender_votacao.c

---
+++ utils/suspender_votacao.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "../structs/Eleitor.h"
+
+bool codigo_projeto_valido(Eleitor comissao[], int *qtde_eleitores) {
+    FILE *arquivo;
+    int votos = 0;
+
+    // Abre o arquivo `parcial.txt` para escrita
+    arquivo = fopen("parcial.txt", "w");
+    if (arquivo == NULL) {
+        printf("Erro ao criar o arquivo parcial.txt\n");
+        return false;
+    }
+
+    // Conta quantos eleitores já votaram
+    for (int i = 0; i < *qtde_eleitores; i++) {
+        if (comissao[i].votou) {
+            votos++;
+        }
+    }
+
+    // Escreve a quantidade de votos no início do arquivo
+    fprintf(arquivo, "%d\n", votos);
+
+    // Escreve o CPF e o código do TG de quem votou
+    for (int i = 0; i < *qtde_eleitores; i++) {
+        if (comissao[i].votou) {
+            fprintf(arquivo, "%s %d\n", comissao[i].cpf, comissao[i].codigo_TG);
+        }
+    }
+
+    // Fecha o arquivo
+    fclose(arquivo);
+    return true;
+}
--- utils/suspender_votacao.h
+++ utils/suspender_votacao.h
@@ -4,47 +4,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+#include "../structs/Eleitor.h"
 
-extern int qtdeEleitores;  // Quantidade de eleitores
-
-// Estrutura do Eleitor, já definida em algum lugar do seu código principal
-typedef struct {
-    char cpf[15];  // CPF do eleitor
-    bool votou;    // true se já votou; false se não votou
-    int codigoTG;  // Código do TG votado (0 se não votou)
-} Eleitor;
-
-bool codigo_projeto_valido(Eleitor comissao[]) {
-    FILE *arquivo;
-    int votos = 0;
-
-    // Abre o arquivo `parcial.txt` para escrita
-    arquivo = fopen("parcial.txt", "w");
-    if (arquivo == NULL) {
-        printf("Erro ao criar o arquivo parcial.txt\n");
-        return false;
-    }
-
-    // Conta quantos eleitores já votaram
-    for (int i = 0; i < qtdeEleitores; i++) {
-        if (comissao[i].votou) {
-            votos++;
-        }
-    }
-
-    // Escreve a quantidade de votos no início do arquivo
-    fprintf(arquivo, "%d\n", votos);
-
-    // Escreve o CPF e o código do TG de quem votou
-    for (int i = 0; i < qtdeEleitores; i++) {
-        if (comissao[i].votou) {
-            fprintf(arquivo, "%s %d\n", comissao[i].cpf, comissao[i].codigoTG);
-        }
-    }
-
-    // Fecha o arquivo
-    fclose(arquivo);
-    return true;
-}
+bool codigo_projeto_valido(Eleitor comissao[]);
 
 #endif
