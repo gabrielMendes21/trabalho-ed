54af2b2612b751c3c58d701bd2532b157860f139
Brigulinoo07 <paulo.silva1449@etec.sp.gov.br>
2024-09-23 15:55:57 -0300

    Gerar resultado

--- utils/gerar_resultado.h
+++ utils/gerar_resultado.h
@@ -4,8 +4,46 @@
 #ifndef GERAR_RESULTADO_H
 #define GERAR_RESULTADO_H
 
-bool gerar_resultado(TG projetos[], Eleitor comissao[], Aluno formandos[], int tamanho_vet)
+bool gerar_resultado(TG projetos[], Eleitor comissao[], Aluno formandos[], professor prof[], int tamanho_proj, int tamanho_eleitor, int tamanho_aluno, int tamanho_professor)
 {
+   
+   TG projeto_mais_votado = projetos[0];
+    // procura projeto mais votado
+    for (int i = 1; i < tamanho_proj; i++) {
+        if (projetos[i].votos > projeto_mais_votado.votos) {
+            projeto_mais_votado = projetos[i];
+        }
+    }
+
+    Eleitor presentes[tamanho_eleitor];
+    Eleitor ausentes[tamanho_eleitor];
+    int num_presentes = 0;
+    int num_ausentes = 0;
+    // procura eleitores presentes e ausentes
+    for (int i = 0; i < tamanho_eleitor; i++) {
+        if (comissao[i].votou) {
+            presentes[num_presentes++] = comissao[i]; // Armazena o eleitor presente
+        } else {
+            ausentes[num_ausentes++] = comissao[i]; // Armazena o eleitor ausente
+        }
+    }
+
+    Aluno aluno_vencedor = formandos[0];
+    //procura o aluno vencedor pelo código
+    for(int i = 1; i < tamanho_aluno; i++){
+        if(projeto_mais_votado.codigo_autor == formandos[i].matricula){
+            aluno_vencedor = formandos[i];
+        }
+    } 
+
+    professor professor_vencedor = prof[0];
+    // procura orientador do projeto vencedor pelo código
+    for(int i = 1; i< tamanho_professor; i++){
+        if(projeto_mais_votado.codigo_orientador==prof[i].codigo){
+            professor_vencedor = prof[i];
+        }
+    }
+
     //abre o arquivo
     FILE *arquivo = fopen("resultado.txt", "w");
     //verifica se o arquivo foi criado
@@ -13,11 +51,22 @@ bool gerar_resultado(TG projetos[], Eleitor comissao[], Aluno formandos[], int t
         printf("Erro ao criar o arquivo\n");
         return false;
     }
-    //escreve o número de votos em cada projeto
-    fprintf(arquivo, "Resultado da votação:\n");
-    for(int i = 0; i < tamanho_vet; i++){
-        fprintf(arquivo, "Projeto: %s, votos: %d\n", projetos[i]. titulo, projetos[i].qtde_votos);
+
+    fprintf(arquivo, "PI VENCEDOR\n");
+    fprintf(arquivo, "Codigo: %d\n", projeto_mais_votado.codigo);
+    fprintf(arquivo, "Titulo: %s\n", projeto_mais_votado.titulo);
+    fprintf(arquivo, "Aluno: %s\n", aluno_vencedor.pes.nome);
+    fprintf(arquivo, "Curso aluno: %s\n", aluno_vencedor.dpto);
+    fprintf(arquivo, "Orientador: %s\n", professor_vencedor.pes.nome);
+    fprintf(arquivo, "Depto orientador: %s\n", professor_vencedor.dpto);
+
+    for(int i = 0; i < num_presentes; i++){
+      fprintf(arquivo, "Eleitores presentes: %s\n",presentes[i].cpf);
     }
+     for(int i = 0; i < num_ausentes; i++){
+      fprintf(arquivo, "Eleitores ausentes: %s\n",ausentes[i].cpf);
+    }
+
 
     fclose(arquivo);
 
